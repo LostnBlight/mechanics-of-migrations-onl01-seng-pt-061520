a738e92fffc4b7ad76f7801080c9851539488e06
Cameron Hudson <camnn3@yahoo.com>
2020-09-06 16:20:08 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -11,10 +11,9 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     def change
       create_table :artists do |t|
         t.string :name
-        t.string
-        t.integer
-        t.string
+        t.string :genre
+        t.integer :age
+        t.string :hometown
       end
     end
-    
-    end
+  end
